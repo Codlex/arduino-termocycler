30ce76c938115b50b07a8cb67ec50ef914b5a304
Dejan Pekter <deximat@gmail.com>
2016-03-13 05:11:20 +0100

    Refactor main to objects.

--- termocycler-main/termocycler-main.ino
+++ termocycler-main/termocycler-main.ino
@@ -1,69 +1,78 @@
+//#include <Keypad.h>
+
+#include "ThermocyclerModel.cpp"
+#include "ThermocyclerView.cpp"
+#include "Logger.cpp"
+
+
+//
+//
+//
+
+//
+//const byte numRows= 4; //number of rows on the keypad
+//const byte numCols= 4; //number of columns on the keypad
+//
+//char keymap[numRows][numCols] = 
+//{
+//  {'1', '2', '3', 'A'}, 
+//  {'4', '5', '6', 'B'}, 
+//  {'7', '8', '9', 'C'},
+//  {'*', '0', '#', 'D'}
+//};
+//
+////Code that shows the the keypad connections to the arduino terminals
+//byte rowPins[numRows] = {2, 3, 4, 5}; //Rows 0 to 3
+//byte colPins[numCols]= {6, 7, 8, 9}; //Columns 0 to 3
+//
+//Keypad myKeypad = Keypad(makeKeymap(keymap), rowPins, colPins, numRows, numCols);
+//
+ThermocyclerModel Model;
+ThermocyclerView View(&Model);
 
 void setup() {
-  initLogger();
-  logDebug("Thermocycler started");
+  Log.init();
+  View.init();
+  Log.debug("Thermocycler started");
 }
 
 void loop() {
-  // do the work
+  View.update();
+  
+//char keypressed = myKeypad.getKey();
+//if (keypressed != NO_KEY)
+//{
+//  char buff[3];
+//  sprintf(buff, "%c", keypressed);
+//  myGLCD.print(buff, CENTER, 250);
+//} else {
+  // Serial.println("none");
+//  }
+  // logDebug("loop");
+  //processInput();
+  // doTheCycling();
+  // updateScreen();
 }
 
+// <Model>
+//const int INITIALIZATION = 0;
+//const int IDLE = 1;
+//
+//int state = INITIALIZATION;
+//
+//struct BathData {
+//  int temperature;
+//} BathData;
+//
+//struct ThermocyclerState {
+//  BathData* leftBath;
+//  BathData* rightBath;
+//  int cycles;
+//};
 
-// <Logger>
-// initial implementation
-// console only
+// </Model>
 
-char loggerBuffer[256];
 
-void initLogger() {
-  Serial.begin(9600);
-}
 
-void logDebug(char* message) {
-  log("DEBUG", message);
-}
 
-void logError(char* message) {
-  log("ERROR", message);
-}
 
-void log(char* tag, char* message) {
-  sprintf(loggerBuffer, "%02d %02d:%02d:%02d,%03d [%s] %.128s\n",
-            days(), hours(), minutes(), seconds(), milliseconds(), tag, message);
-  Serial.print(loggerBuffer);
-}
-
-// </Logger>
-
-// <Time>
-// realtive time since startup
-// TODO: reset after 50 days
-
-const int millisecondsInSecond = 1000;
-const int secondsInMinute = 60;
-const int minutesInHour = 60;
-const int hoursInDay = 24;
-
-int days() {
-  int divider = millisecondsInSecond * secondsInMinute * minutesInHour * hoursInDay;
-  return millis() / divider;
-}
-
-int hours() {
-  int divider = millisecondsInSecond * secondsInMinute * minutesInHour;
-  return (millis() / divider) % hoursInDay;
-}
-
-int minutes() {
-  int divider = millisecondsInSecond * secondsInMinute;
-  return (millis() / divider) % minutesInHour;
-}
-
-int seconds() {
-  return (millis() / millisecondsInSecond) % secondsInMinute;
-}
-
-int milliseconds() {
-  return millis() % millisecondsInSecond;
-}
-// </Time>
