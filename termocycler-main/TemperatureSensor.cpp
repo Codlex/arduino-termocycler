eb5c3d4c8c1e3ca86ec46345fe35919334c9fd04
Dejan Pekter <deximat@gmail.com>
2016-03-15 18:01:25 +0100

    Simple TemperatureSensor logic.

--- termocycler-main/TemperatureSensor.cpp
+++ termocycler-main/TemperatureSensor.cpp
@@ -1,10 +1,32 @@
 #ifndef TEMPERATURE_SENSOR_H
 #define TEMPERATURE_SENSOR_H
 
+#include "Arduino.h"
+
 class TemperatureSensor {
+  private: 
+    int sensorPin;
+    float getVoltage() {
+      int reading = analogRead(sensorPin);
+      float maxVoltage = 5.0; // for 3.3V should be 3.3
+      float voltage = reading * 5.0;
+      voltage /= 1024.0;
+      return voltage;
+    }
+
+    static float voltageToTemperature(float voltage) {
+      //converting from 10 mv per degree wit 500 mV offset
+      // to degrees ((voltage - 500mV) times 100)
+      return (voltage - 0.5) * 100 ;
+    }
+    
   public:
+    TemperatureSensor(int sensorPin) {
+      this->sensorPin = sensorPin;
+    }
+    
     int getTemperature() {
-      // DO SOMETHING
+      return voltageToTemperature(getVoltage());
     }
 };
 
