f048f10d258ac4efd27af53dbc6ca82008a13cc0
Dejan Pekter <deximat@gmail.com>
2016-05-16 21:36:05 +0200

    Switched switches current.

--- termocycler-main/logic/Switch.cpp
+++ termocycler-main/logic/Switch.cpp
@@ -5,11 +5,17 @@
 
 class Switch {
 
+
+
 	enum SwitchState {
 		On, Off
 	};
 
 private:
+
+	static const int ON_CURRENT = LOW;
+	static const int OFF_CURRENT = HIGH;
+
 	SwitchState state = Off;
 	int pin;
 
@@ -21,13 +27,13 @@ public:
 	Switch(int pin) {
 		this->pin = pin;
 		pinMode(this->pin, OUTPUT);
-		digitalWrite(this->pin, LOW);
+		digitalWrite(this->pin, OFF_CURRENT);
 	}
 
 	void turnOn() {
 		if (this->state != On) {
 			this->state = On;
-			digitalWrite(this->pin, HIGH);
+			digitalWrite(this->pin, ON_CURRENT);
 			on();
 		}
 	}
@@ -35,7 +41,7 @@ public:
 	void turnOff() {
 		if (this->state != Off) {
 			this->state = Off;
-			digitalWrite(this->pin, LOW);
+			digitalWrite(this->pin, OFF_CURRENT);
 			off();
 		}
 	}
